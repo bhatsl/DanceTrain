032d1113407fcd30ebab98bce6ec818c82889c7f
bhatsl <bhatsleeh10@gmail.com>
2025-10-09 13:44:08 -0300

    ajustes

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBVEB6ytTXsuM7ABMaO1G8LeYhz3YoGvIs",
+  authDomain: "dancetrain-64c7a.firebaseapp.com",
+  projectId: "dancetrain-64c7a",
+  storageBucket: "dancetrain-64c7a.firebasestorage.app",
+  messagingSenderId: "351474949291",
+  appId: "1:351474949291:web:96d51933424c72d9dbebee"
+};
