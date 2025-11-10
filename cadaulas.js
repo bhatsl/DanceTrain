195727c63082cce4605f6929e38d06d6d1c950d2
edertec <edertec@msn.com>
2025-11-10 13:27:10 -0300

    ajustes

--- cadaulas.js
+++ cadaulas.js
@@ -1,24 +1,22 @@
-function cadastrar(event) {
-    event.preventDefault();
-
-    var data = document.getElementById("data_da_aula").value;
-    var nome = document.getElementById("nome_da_aula").value;
-    var estilo = document.getElementById("estilo_de_dança").value;
-    var anotacoes = document.getElementById("anotações").value;
+function cadastrar() {
+    var data = document.getElementById("data").value;
+    var nome = document.getElementById("nome").value;
+    var estilo = document.getElementById("estilo").value;
+    var anotacoes = document.getElementById("anotacoes").value;
 
     const aulaNova = {
-        data: data_da_aula,
-        nome: nome_da_aula,
-        estilo: estilo_de_dança,
-        anotacoes: anotações,
+        data: data,
+        nome: nome,
+        estilo: estilo,
+        anotacoes: anotacoes,
     };
 
     db.ref('aulas').push(aulaNova)
         .then(() => {
             alert("Registrado com sucesso! ");
-            document.getElementById('data_da_aula').value = '';
-            document.getElementById('nome_da_aula').value = '';
-            document.getElementById('estilo_de_dança').value = '';
-            document.getElementById('anotações').value = '';
+            document.getElementById('data').value = '';
+            document.getElementById('nome').value = '';
+            document.getElementById('estilo').value = '';
+            document.getElementById('anotacoes').value = '';
         });
  }
